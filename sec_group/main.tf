43c8d67fe12481b8beb1db7d7845bb17c0d1bf9f
rkalit <biboo@wrinkled-oyaji>
2024-08-01 00:18:28 +0700

    separating between ingress and egress rules

--- sec_group/main.tf
+++ sec_group/main.tf
@@ -3,14 +3,22 @@ resource "alicloud_security_group" "this" {
   vpc_id = var.vpc_id
 }
 
-resource "alicloud_security_group_rule" "this" {
+resource "alicloud_security_group_rule" "ingress" {
+  count             = length(var.ingress_rules)
   security_group_id = alicloud_security_group.this.id
-  type              = var.rule_type
-  ip_protocol       = var.protocol
-  nic_type          = var.nic_type
-  policy            = var.policy
-  port_range        = var.port_range
-  priority          = var.priority
-  cidr_ip           = var.cidr_ip
-  description       = var.description
+  type              = "ingress"
+  ip_protocol       = var.ingress_rules[count.index].protocol
+  policy            = var.ingress_rules[count.index].policy
+  port_range        = var.ingress_rules[count.index].port_range
+  cidr_ip           = var.ingress_rules[count.index].cidr_ip
+}
+
+resource "alicloud_security_group_rule" "egress" {
+  count             = length(var.egress_rules)
+  security_group_id = alicloud_security_group.this.id
+  type              = "egress"
+  ip_protocol       = var.egress_rules[count.index].protocol
+  policy            = var.egress_rules[count.index].policy
+  port_range        = var.egress_rules[count.index].port_range
+  cidr_ip           = var.egress_rules[count.index].cidr_ip
 }
--- sec_group/variables.tf
+++ sec_group/variables.tf
@@ -8,48 +8,24 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "rule_type" {
-  description = "variable for rule_type on security group rule"
-  type        = string
-  default     = "ingress"
+variable "ingress_rules" {
+  description = "List of the ingress rules to apply on the security group rules"
+  type = list(object({
+    protocol   = string
+    port_range = string
+    policy     = string
+    cidr_ip    = string
+  }))
+  default = []
 }
 
-variable "protocol" {
-  description = "variable for ip protocol on security group rule"
-  type        = string
-  default     = "tcp"
-}
-
-variable "nic_type" {
-  description = "variable for nic type on security group rule (should set to intranet when security group type is vpc)"
-  type        = string
-  default     = "intranet"
-}
-
-variable "policy" {
-  description = "policy for security group, either to accept or to deny request"
-  type        = string
-}
-
-variable "port_range" {
-  description = "variable for port range for security group"
-  type        = string
-}
-
-variable "priority" {
-  description = "variable for priority on the security group rule"
-  type        = number
-  default     = 1
-}
-
-variable "cidr_ip" {
-  description = "variable for the ip where the request come from"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "description" {
-  description = "variable for security group roles' description"
-  type = string
-  default = ""
+variable "egress_rules" {
+  description = "List of the egress rules to apply on the security group rules"
+  type = list(object({
+    protocol   = string
+    port_range = string
+    policy     = string
+    cidr_ip    = string
+  }))
+  default = []
 }
